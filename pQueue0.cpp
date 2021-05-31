61a24dd587cf1c01a7138ffb969813906225aab6
Andrew Fischer <agejfischer@gmail.com>
2021-05-31 07:26:17 -0400

    Untested implementation of moveUp() method

--- pQueue0.cpp
+++ pQueue0.cpp
@@ -4,7 +4,6 @@ PQueue::PQueue() {
     _size = 0;
 }
 
-// Remember to call moveUp() after insertion
 void PQueue::insert(T val) {
     if (_size == 0) {
         _array[1] == val;
@@ -12,11 +11,36 @@ void PQueue::insert(T val) {
     } else if (_size < (MAX_SIZE)) {
         _array[_size + 1] = val;
         _size++;
+        moveUp();   // Fix heap since heap property likely violated.
     } else {    // Heap is full, nothing can be inserted.
         return;
     }
 }
 
+// If a child is less than its parent, swap positions since min heap property is violated.
+void PQueue::moveUp() {
+    int childIndex = _size;
+    int parentIndex = _size / 2;   // int division finds parent index since flooring occurs
+    int childVal = _array[childIndex];
+    int parentVal = _array[parentIndex];
+    int tempVal;
+    int tempIndex;
+    while (childVal < parentVal) {
+        tempVal = parentVal;
+        parentVal = childVal;
+        childVal = tempVal;
+        childIndex = parentIndex;
+        parentIndex = parentIndex / 2;
+        childVal = _array[childIndex];
+        parentVal = _array[parentIndex];
+        if (parentIndex == 0) {
+            return;
+        }
+    }
+    return;
+}
+
+/*
 T PQueue::findMin() {
     if (_size != 0) {
         return _array[1];
@@ -24,3 +48,4 @@ T PQueue::findMin() {
         return;
     }
 }
+*/
